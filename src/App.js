5031ab01011b3c65aa3da58818b8e8c8a8528f0f
Angel <agarcia261@gmail.com>
2019-03-02 19:41:32 -0500

    final version

--- src/App.js
+++ src/App.js
@@ -8,15 +8,84 @@ import Gameinfo from "./components/Gameinfo";
 
 
 class App extends Component {
+   // Setting the component's initial state
+   state = {
+    score: 0,
+    topScore: 0,
+    images:[]
+  };
+
+  images = [
+    {id:1, src:"/assets/images/ironman.png", clicked:false},
+    {id:2, src:"/assets/images/blackpanther.png", clicked:false},
+    {id:3, src:"/assets/images/blackwidow.png", clicked:false},
+    {id:4, src:"/assets/images/captain.png", clicked:false},
+    {id:5, src:"/assets/images/drstrange.png", clicked:false},
+    {id:6, src:"/assets/images/falcon.png", clicked:false},
+    {id:7, src:"/assets/images/hawk.png", clicked:false},
+    {id:8, src:"/assets/images/hulk.png", clicked:false},
+    {id:9, src:"/assets/images/vision.png", clicked:false},
+    {id:10, src:"/assets/images/peter.png", clicked:false},
+    {id:11, src:"/assets/images/spiderman.png", clicked:false},
+    {id:12, src:"/assets/images/thor.png", clicked:false},
+  ];
+
+  handleClick = id => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    // event.preventDefault();
+    let imgIndex = -1
+    this.state.images.forEach( function (img, index){
+      if (img.id == id){
+        imgIndex=index
+      }
+    })
+    if (this.images[imgIndex].clicked){
+      this.lostGame()
+    }
+    else{
+      this.images[imgIndex].clicked=true
+      if (this.state.score>=this.state.topScore || this.state.topScore===0){
+       this.setState({ score: this.state.score + 1, topScore: this.state.topScore +1 })
+      }
+      else{
+        this.setState({ score: this.state.score + 1})
+      }
+      this.generateRandomImgs()
+
+    }
+  };
+  generateRandomImgs = () => {
+    let imagesRdm = this.images.sort(() => 0.5 - Math.random())
+    this.setState({images:imagesRdm});
+  }
+  lostGame = () => {
+    this.setState({score: 0})
+    this.images.forEach(function (img){
+      img.clicked = false
+    })
+    this.generateRandomImgs()
+
+  }
+  componentDidMount() {
+      this.generateRandomImgs()
+  }
   render() {
     return (
       <Router>
         <div>
-          <Nav />
+          <Nav 
+            score = {this.state.score}
+            topScore = {this.state.topScore}
+          />
           <Gameinfo />
-          <Route exact path="/" component={Main} />
+          <Main 
+            images = {this.state.images}
+            handleClick = {this.handleClick}
+          />
+          {/* <Route exact path="/" component={Main} /> */}
         </div>
-      </Router>      
+      </Router>   
+      
 
     );
   }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./style.css";
 
-function Nav() {
+function Nav(props) {
   return (
     <nav className="navbar">
             <div className="nav-items col-md-4">
                 <h1><a class="nav-link" href="/">Clicky Game</a></h1>
             </div>
             <div className="nav-items col-md-4"><h1>Click an image to begin!</h1></div>
-            <div className="nav-items col-md-4"><h1>Scores go here!</h1></div>
+            <div className="nav-items col-md-4"><h1>Scores: {props.score} | Top Score: {props.topScore}</h1></div>
     </nav>
   );
 }
