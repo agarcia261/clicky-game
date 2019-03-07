5ad974a27f805d3deb31d40f5dea9fe2e078ca25
Angel <agarcia261@gmail.com>
2019-03-06 21:25:40 -0500

    Updates

---
+++ precache-manifest.4473c29f3c836ccdef7938088685d9bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "5a0322540d65fa24a839",
+    "url": "/clicky-game/static/js/main.5a032254.chunk.js"
+  },
+  {
+    "revision": "f86ab67e1753a0c865de",
+    "url": "/clicky-game/static/js/2.f86ab67e.chunk.js"
+  },
+  {
+    "revision": "5a0322540d65fa24a839",
+    "url": "/clicky-game/static/css/main.c5761932.chunk.css"
+  },
+  {
+    "revision": "e0c999e5c9b3affbd152d256cbace376",
+    "url": "/clicky-game/index.html"
+  }
+];
